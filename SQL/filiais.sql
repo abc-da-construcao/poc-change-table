45f0199738c9838d2b032a142accfe7fde0c964d
joao.raminelli <joao.raminelli@abcdaconstrucao.com.br>
2023-05-30 17:19:35 -0300

    adicionado colunas last_operation e last_commit_time na tabela FILIALCAD

--- SQL/filiais.sql
+++ SQL/filiais.sql
@@ -1,10 +1,12 @@
-
 SELECT
         f.codempresa,
         f.filial,
         f.nome,
         f.cgc,
         f.oidempresa,
-        f.oid
+        ct.oid,
+        ct.SYS_CHANGE_OPERATION AS last_operation,
+        COALESCE(tc.commit_time, GETDATE()) AS last_commit_time
 FROM CHANGETABLE (CHANGES [FILIALCAD], :lastVersion) AS ct
-JOIN FILIALCAD f on f.oid = ct.oid
+LEFT JOIN sys.dm_tran_commit_table tc on ct.sys_change_version = tc.commit_ts
+LEFT JOIN FILIALCAD f on f.oid = ct.oid
