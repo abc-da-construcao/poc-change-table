ece1a5cdb56672172715dd8a694ad08d8a237dce
joao.raminelli <joao.raminelli@abcdaconstrucao.com.br>
2023-05-31 10:30:11 -0300

    adicionado colunas last_operation e last_commit_time na tabela ITEMFILEST

--- database/migrations/2023_04_14_190346_create_estoques_table.php
+++ database/migrations/2023_04_14_190346_create_estoques_table.php
@@ -15,13 +15,15 @@ return new class extends Migration {
         Schema::create('estoques', function (Blueprint $table) {
             $table->id();
             $table->integer('codpro');
-            $table->string('dv');
-            $table->string('filial',2);
-            $table->string('referencia');
+            $table->string('dv')->nullable(true);
+            $table->string('filial',2)->nullable(true);
+            $table->string('referencia')->nullable(true);
             $table->double('estoque_atual', 8, 3)->nullable(true);
             $table->double('estoque_futuro', 8, 3)->nullable(true);
             $table->smallInteger('compras_1')->nullable(true)->comment('compra 15 dias')->default(0);
             $table->smallInteger('compras_2')->nullable(true)->comment('compra 45 dias')->default(0);
+            $table->string('last_operation', 1);
+            $table->dateTime('last_commit_time');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
 
